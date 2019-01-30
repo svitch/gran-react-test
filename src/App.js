2ab0098a8fca0e6cbdeaf61135b586b034120bf8
Sergey Sklyar <svitch@gmail.com>
2019-01-30 09:36:15 +0300

    layout, router and styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Index = () => <h1>HomePage</h1>;
+const Item = ({ match }) => <p>This is {match.params.id} Page</p>;
+const Items = ({ match }) => (
+  <section className="section">
+    <div className="row">
+      <aside className="col-auto aside">
+        <nav className="menu">
+          <NavLink to={`${match.url}`} exact className="nav-link" activeClassName="active">Info</NavLink>
+          <NavLink to={`${match.url}/item1`} className="nav-link" activeClassName="active">Item1</NavLink>
+          <NavLink to={`${match.url}/item2`} className="nav-link" activeClassName="active">Item2</NavLink>
+        </nav>
+      </aside>
+      <article className="col article">
+        <Route path={`${match.path}/:id`} component={Item} />
+        <Route exact path={match.path} render={() => <p>Items Root Page</p>} />
+      </article>
+    </div>
+  </section>
+);
+
+const App = () => (
+  <Router>
+    <div id="page">
+      <header id="header">
+        <div className="container">
+          <ul className="nav">
+            <li><NavLink className="nav-link" to="/" exact activeClassName="active">Home</NavLink></li>
+            <li><NavLink className="nav-link" to="/items" activeClassName="active">Items</NavLink></li>
+          </ul>
+        </div>
+      </header>
+      <main id="main">
+        <div className="container">
+          <Route path="/" exact component={Index} />
+          <Route path="/items" component={Items} />
+        </div>
+      </main>
+      <footer id="footer">
+        <div className="container">
+          Built with ReactJS by Sergey Sklyar
+        </div>
+      </footer>
+    </div>
+  </Router>
+);
 
 export default App;
